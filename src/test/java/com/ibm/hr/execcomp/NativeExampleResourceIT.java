e7139c075138f78a26db4175b253b919390ecd94
quarkusio <no-reply@quarkus.io>
2020-06-25 07:23:29 +0000

    Initial commit

---
+++ src/test/java/com/ibm/hr/execcomp/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.ibm.hr.execcomp;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
